b9e93f8aac8201e8e1d08382490a1fe07db48d3e
sneakers-the-rat <JLSaunders987@gmail.com>
2023-04-27 11:53:49 -0700

    feature 1

--- src/myfunction.py
+++ src/myfunction.py
@@ -2,7 +2,9 @@
 These are my functions
 """
 
-def add(x:int, y:int) -> int:
-    return x+y
+def add(x:int, y:int, _round:bool=True) -> int:
+    if _round:
+        return round(x+y)
+    else: 
+        return x+y
 
-    # whoops
